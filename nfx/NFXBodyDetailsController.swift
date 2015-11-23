30382f26c9df1f0e7187a8ce40ea7a8973937a3d
Vicente Crespo <vicente.crespo.penades@gmail.com>
2015-11-23 21:37:01 +0100

    - Pretty print in response body details :D

--- nfx/NFXBodyDetailsController.swift
+++ nfx/NFXBodyDetailsController.swift
@@ -12,7 +12,7 @@ class NFXBodyDetailsController : UIViewController
 {
     var bodyView: UITextView = UITextView()
     var iIndex: Int = 0
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -24,15 +24,54 @@ class NFXBodyDetailsController : UIViewController
         self.view.backgroundColor = UIColor.init(netHex: 0xf2f2f2)
         
         let tempObject = NFXHTTPModelManager.sharedInstance.models[self.iIndex]
-
+        
         self.bodyView.frame = CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame))
         self.bodyView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         self.bodyView.backgroundColor = UIColor.clearColor()
         self.bodyView.textColor = UIColor.init(netHex: 0x707070)
         self.bodyView.editable = false
-        self.bodyView.text = tempObject.getResponseBody() as String!
+        
+        
+        // Added by Vicente Crespo Penadés
+        // vicente.crespo.penades@gmail.com - 23 Nov 2015
+        let responseBody = prettyStringIfPossibleWithResponse( tempObject.getResponseBody() as String)
+        
+        
+        self.bodyView.text = responseBody
+        
         self.view.addSubview(self.bodyView)
         
+    }
+    
+    
+    //
+    // MARK: Utils
+    //
+    
+    private func prettyStringIfPossibleWithResponse(response: String?) -> String? {
+        
+        guard let responseBody = response
+            else { return nil }
+        
+        do {
+            if let responseBodyData = responseBody.dataUsingEncoding(NSUTF8StringEncoding) {
+                
+                let responseObj = try NSJSONSerialization.JSONObjectWithData(responseBodyData,
+                    options: .AllowFragments)
+                
+                let dataResponse = try NSJSONSerialization.dataWithJSONObject(responseObj,
+                    options: .PrettyPrinted)
+                
+                let responseBodyPretty = String(data: dataResponse,
+                    encoding: NSUTF8StringEncoding)
+                
+                return responseBodyPretty
+            }
+        } catch {
+            return response
+        }
+        
+        return response
     }
     
 }
