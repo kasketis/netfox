40401af938660cecc29be3edb8fe04f725960ddd
Christos Kasketis <user@users-imac.exodussa.gr>
2015-11-30 16:05:46 +0200

    CHG: Var names

--- netfox/NFXGenericController.swift
+++ netfox/NFXGenericController.swift
@@ -29,30 +29,23 @@ class NFXGenericController: UIViewController
     
     func formatNFXString(string: String) -> NSAttributedString
     {
-        let l = string.characters.count
-        
-        let regex1 = try! NSRegularExpression(pattern: "(\\-- Body \\--)|(\\-- Headers \\--)", options: NSRegularExpressionOptions.CaseInsensitive)
-        
-        
-        let matches1 = regex1.matchesInString(string, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, l)) as Array<NSTextCheckingResult>
-        
         var tempMutableString = NSMutableAttributedString()
-        
         tempMutableString = NSMutableAttributedString(string: string)
         
-        for match in matches1 {
+        let l = string.characters.count
+        
+        let regexBodyHeaders = try! NSRegularExpression(pattern: "(\\-- Body \\--)|(\\-- Headers \\--)", options: NSRegularExpressionOptions.CaseInsensitive)
+        let matchesBodyHeaders = regexBodyHeaders.matchesInString(string, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, l)) as Array<NSTextCheckingResult>
+        
+        for match in matchesBodyHeaders {
             tempMutableString.addAttribute(NSFontAttributeName, value: UIFont.NFXFontBold(13), range: match.range)
             tempMutableString.addAttribute(NSForegroundColorAttributeName, value: UIColor.NFXOrangeColor(), range: match.range)
         }
         
+        let regexKeys = try! NSRegularExpression(pattern: "\\[.+?\\]", options: NSRegularExpressionOptions.CaseInsensitive)
+        let matchesKeys = regexKeys.matchesInString(string, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, l)) as Array<NSTextCheckingResult>
         
-        let regex2 = try! NSRegularExpression(pattern: "\\[.+?\\]", options: NSRegularExpressionOptions.CaseInsensitive)
-        
-        
-        let matches2 = regex2.matchesInString(string, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, l)) as Array<NSTextCheckingResult>
-        
-        
-        for match in matches2 {
+        for match in matchesKeys {
             tempMutableString.addAttribute(NSForegroundColorAttributeName, value: UIColor.NFXBlackColor(), range: match.range)
         }
         
@@ -62,6 +55,5 @@ class NFXGenericController: UIViewController
     
     func reloadData()
     {
-
     }
 }
