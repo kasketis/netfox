cd96954d4a9139973dfa6a4aacbf5c348045af7e
Jangles <njangula@myriadmobile.com>
2017-10-12 12:20:07 -0500

    Added an iOS demo

---
+++ netfox_ios_demo/TextViewController.swift
@@ -0,0 +1,72 @@
+//
+//  TextViewController.swift
+//  netfox_ios_demo
+//
+//  Created by Nathan Jangula on 10/12/17.
+//  Copyright © 2017 kasketis. All rights reserved.
+//
+
+import UIKit
+import netfox_ios
+
+class TextViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    let session: URLSession!
+    var dataTask: URLSessionDataTask?
+    
+    required init?(coder aDecoder: NSCoder) {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses?.insert(NFXProtocol.self, at: 0)
+        session = URLSession(configuration: configuration)
+        
+        super.init(coder: aDecoder)
+    }
+    
+    @IBAction func tappedLoad(_ sender: Any) {
+        dataTask?.cancel()
+        
+        if let url = URL(string: "https://api.chucknorris.io/jokes/random") {
+            dataTask = session.dataTask(with: url, completionHandler: { (data, response, error) in
+                if let error = error {
+                    self.handleCompletion(error: error.localizedDescription, data: data)
+                } else {
+                    guard let data = data else { self.handleCompletion(error: "Invalid data", data: nil); return }
+                    guard let response = response as? HTTPURLResponse else { self.handleCompletion(error: "Invalid response", data: data); return }
+                    guard response.statusCode >= 200 && response.statusCode < 300 else { self.handleCompletion(error: "Invalid response code", data: data); return }
+                    
+                    self.handleCompletion(error: error?.localizedDescription, data: data)
+                }
+            })
+            
+            dataTask?.resume()
+        }
+    }
+    
+    private func handleCompletion(error: String?, data: Data?) {
+        DispatchQueue.main.async {
+            
+            if let error = error {
+                NSLog(error)
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let dict = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                    
+                    if let message = dict?["value"] as? String {
+                        self.textView.text = message
+                    }
+                    
+                    //                let image = UIImage(data: data)
+                    //                NSLog("\(image?.size.width ?? 0),\(image?.size.height ?? 0)")
+                    //                self.imageView.image = image
+                } catch {
+                    
+                }
+            }
+        }
+    }
+}
+
