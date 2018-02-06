f670668343a77607a0b56095b0681b97f1637e6f
Ștefan Suciu <stefan.suciu@tapptitude.com>
2018-02-06 11:33:52 +0200

    removed a bad assignment at node creation

--- netfox/Core/NFXPathNodeManager.swift
+++ netfox/Core/NFXPathNodeManager.swift
@@ -8,18 +8,16 @@
 
 import Foundation
 
-final class NFXPathNodeManager: NSObject
-{
+final class NFXPathNodeManager: NSObject {
+    
     static let sharedInstance = NFXPathNodeManager()
     fileprivate var rootNode = NFXPathNode(name: "root")
     
-    func add(_ arr: [NFXHTTPModel])
-    {
+    func add(_ arr: [NFXHTTPModel]) {
         arr.forEach{ add($0) }
     }
     
-    func add(_ obj: NFXHTTPModel)
-    {
+    func add(_ obj: NFXHTTPModel) {
         guard let nodes = obj.requestURL?.split(separator: "/").dropFirst().map({ NFXPathNode(name: String($0)) }) else {
             return
         }
@@ -30,7 +28,6 @@ final class NFXPathNodeManager: NSObject
             if let foundNode = previousNode.find(node) {
                 previousNode = foundNode
             } else {
-                node.httpModel = obj
                 previousNode.insert(node)
                 previousNode = node
             }
