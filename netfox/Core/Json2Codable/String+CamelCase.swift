d9b8194bb07b1ded2d5559ed215cd93df653ca20
Ștefan Suciu <stefan.suciu@tapptitude.com>
2018-02-14 17:12:04 +0200

    some code refactoring + replacing last occurence

--- netfox/Core/Json2Codable/String+CamelCase.swift
+++ netfox/Core/Json2Codable/String+CamelCase.swift
@@ -11,25 +11,47 @@ import Foundation
 extension String {
     
     func uppercaseFirstLetter() -> String {
-        let paranthesisCount = components(separatedBy: "]").count - 1
-        let stringWithoutParanthesis = dropFirst(paranthesisCount)
-        
-        return String(repeating: "[", count: paranthesisCount) + stringWithoutParanthesis.prefix(1).capitalized + stringWithoutParanthesis.dropFirst()
+        if hasPrefix("[") && hasSuffix("]") {
+            let leadingParanthesisCount = components(separatedBy: "[").count - 1
+            let endingParanthesisCount = components(separatedBy: "]").count - 1
+            let startIndex = index(self.startIndex, offsetBy: leadingParanthesisCount)
+            let endIndex = index(self.endIndex, offsetBy: -endingParanthesisCount)
+            let stringWithoutParanthesis = self[startIndex..<endIndex]
+            let leadingParanthesis = String(repeating: "[", count: leadingParanthesisCount)
+            let endingParanthesis = String(repeating: "]", count: endingParanthesisCount)
+            
+            return leadingParanthesis + stringWithoutParanthesis.prefix(1).capitalized + stringWithoutParanthesis.dropFirst() + endingParanthesis
+        } else {
+            return prefix(1).capitalized + dropFirst()
+        }
     }
     
     func lowercaseFirstLetter() -> String {
         return prefix(1).lowercased() + dropFirst()
     }
     
+    func replacingLastOccurences(of searchString: String, with replacementString: String) -> String {
+        if let range = self.range(of: searchString, options: [.backwards, .caseInsensitive]) {
+            return self.replacingCharacters(in: range, with: replacementString)
+        }
+        
+        return self
+    }
+    
     var camelCasedString: String {
-        return components(separatedBy: CharacterSet(charactersIn: "_- ")).map{ $0.uppercaseFirstLetter() }.joined()
+        let splittedComponents = components(separatedBy: CharacterSet(charactersIn: "_- "))
+        return splittedComponents.first! + splittedComponents.dropFirst().map{ $0.uppercaseFirstLetter() }.joined()
     }
     
     var singular: String {
         let paranthesisCount = components(separatedBy: "]").count - 1
         let stringWithoutParanthesis = dropLast(paranthesisCount)
         
-        if stringWithoutParanthesis.last == "s" {
+        if stringWithoutParanthesis.hasSuffix("ies") {
+            return replacingLastOccurences(of: "ies", with: "y")
+        }
+        
+        if stringWithoutParanthesis.hasSuffix("s") {
             return stringWithoutParanthesis.dropLast() + String(repeating: "]", count: paranthesisCount)
         }
         
