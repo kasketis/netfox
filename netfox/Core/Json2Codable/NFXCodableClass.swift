5ffb41856f9fdf4657beb0ce056b3027c2c23806
Ștefan Suciu <stefan.suciu@tapptitude.com>
2018-03-08 09:15:29 +0200

    show in coding keys only properties which have different name than the key

--- netfox/Core/Json2Codable/NFXCodableClass.swift
+++ netfox/Core/Json2Codable/NFXCodableClass.swift
@@ -67,7 +67,7 @@ class NFXCodableClass: CustomStringConvertible {
         var str = "class \(getClassName(className)): Codable {\n\n"
         str += properties.map{ "\tvar \(getPropertyName($0.name)): \(getPropertyType($0.type))\(getOptional($0.isOptional))" }.joined(separator: "\n")
         str += "\n\n\tenum CodingKeys: String, CodingKey {\n"
-        str += properties.map{"\t\tcase \(getPropertyName($0.name)) = \"\($0.name)\""}.joined(separator: "\n")
+        str += properties.filter{ getPropertyName($0.name) != $0.name }.map{"\t\tcase \(getPropertyName($0.name)) = \"\($0.name)\""}.joined(separator: "\n")
         str += "\n\t}"
         str += "\n}\n\n"
         return str
