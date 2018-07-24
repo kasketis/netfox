fdfc18a69fa8d436f515189413ec98bd8d19f6c0
Alex Bofu <>
2018-07-24 10:35:34 +0300

    removed the necessity of importing and init-ing netfox

---
+++ netfox/Core/NSURLSessionConfiguration+NFX.h
@@ -0,0 +1,17 @@
+//
+//  NSURLSessionConfiguration+NFX.h
+//  netfox_ios
+//
+//  Created by Alex Bofu on 23/07/2018.
+//  Copyright © 2018 kasketis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSURLSessionConfiguration (NFX)
+
+@end
+
+NS_ASSUME_NONNULL_END
