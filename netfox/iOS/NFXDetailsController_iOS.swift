d966f4ea8a495cc21dc00e85c2437096e3c93f06
Petr Pražák <petr.prazak@avast.com>
2018-08-16 23:49:01 +0200

    Share data using activity controller

--- netfox/iOS/NFXDetailsController_iOS.swift
+++ netfox/iOS/NFXDetailsController_iOS.swift
@@ -177,12 +177,12 @@ class NFXDetailsController_iOS: NFXDetailsController, MFMailComposeViewControlle
         actionSheetController.addAction(cancelAction)
         
         let simpleLog: UIAlertAction = UIAlertAction(title: "Simple log", style: .default) { [unowned self] action -> Void in
-            self.sendMailWithBodies(false)
+            self.shareLog(full: false)
         }
         actionSheetController.addAction(simpleLog)
         
         let fullLogAction: UIAlertAction = UIAlertAction(title: "Full log", style: .default) { [unowned self] action -> Void in
-            self.sendMailWithBodies(true)
+            self.shareLog(full: true)
         }
         actionSheetController.addAction(fullLogAction)
         actionSheetController.view.tintColor = UIColor.NFXOrangeColor()
@@ -269,52 +269,39 @@ class NFXDetailsController_iOS: NFXDetailsController, MFMailComposeViewControlle
         return bodyDetailsController
     }
     
-    func sendMailWithBodies(_ bodies: Bool)
+    func shareLog(full: Bool)
     {
-        if (MFMailComposeViewController.canSendMail()) {
-            
-            let mailComposer = MFMailComposeViewController()
-            mailComposer.mailComposeDelegate = self
-            
-            var tempString: String
-            tempString = String()
-            
-            
-            tempString += "** INFO **\n"
-            tempString += "\(getInfoStringFromObject(self.selectedModel).string)\n\n"
-            
-            tempString += "** REQUEST **\n"
-            tempString += "\(getRequestStringFromObject(self.selectedModel).string)\n\n"
-            
-            tempString += "** RESPONSE **\n"
-            tempString += "\(getResponseStringFromObject(self.selectedModel).string)\n\n"
-            
-            tempString += "logged via netfox - [https://github.com/kasketis/netfox]\n"
-            
-            mailComposer.setSubject("netfox log - \(self.selectedModel.requestURL!)")
-            mailComposer.setMessageBody(tempString, isHTML: false)
-            
-            if bodies {
-                let requestFilePath = self.selectedModel.getRequestBodyFilepath()
-                if let requestFileData = try? Data(contentsOf: URL(fileURLWithPath: requestFilePath as String)) {
-                    mailComposer.addAttachmentData(requestFileData, mimeType: "text/plain", fileName: "request-body")
-                }
-                
-                let responseFilePath = self.selectedModel.getResponseBodyFilepath()
-                if let responseFileData = try? Data(contentsOf: URL(fileURLWithPath: responseFilePath as String)) {
-                    mailComposer.addAttachmentData(responseFileData, mimeType: "text/plain", fileName: "response-body")
-                }
+        var tempString = String()
+
+        tempString += "** INFO **\n"
+        tempString += "\(getInfoStringFromObject(self.selectedModel).string)\n\n"
+
+        tempString += "** REQUEST **\n"
+        tempString += "\(getRequestStringFromObject(self.selectedModel).string)\n\n"
+
+        tempString += "** RESPONSE **\n"
+        tempString += "\(getResponseStringFromObject(self.selectedModel).string)\n\n"
+
+        tempString += "logged via netfox - [https://github.com/kasketis/netfox]\n"
+
+        if full {
+            let requestFilePath = self.selectedModel.getRequestBodyFilepath()
+            if let requestFileData = try? String(contentsOf: URL(fileURLWithPath: requestFilePath as String), encoding: .utf8) {
+                tempString += requestFileData
             }
 
-            self.present(mailComposer, animated: true, completion: nil)
+            let responseFilePath = self.selectedModel.getResponseBodyFilepath()
+            if let responseFileData = try? String(contentsOf: URL(fileURLWithPath: responseFilePath as String), encoding: .utf8) {
+                tempString += responseFileData
+            }
         }
+        displayShareSheet(shareContent: tempString)
     }
-    
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?)
-    {
-        self.dismiss(animated: true, completion: nil)
+
+    func displayShareSheet(shareContent: String) {
+        let activityViewController = UIActivityViewController(activityItems: [shareContent as NSString], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: {})
     }
-    
 }
 
 #endif
