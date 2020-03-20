40ead7452096df961303e7a78ba75145bc3ba5ff
Stefan Haider <stefan.haider@upstream-mobility.at>
2020-03-20 09:31:19 +0100

    changed nfxloader to swift

---
+++ netfox/Core/NFXLoader.swift
@@ -0,0 +1,18 @@
+//
+//  NFXLoader.swift
+//  netfox_ios
+//
+//  Created by Stefan Haider on 20.03.20.
+//  Copyright © 2020 kasketis. All rights reserved.
+//
+
+import UIKit
+
+class NFXLoader {
+    class func load() {
+        let implementNetfoxSelector = NSSelectorFromString("implementNetfox")
+        if URLSessionConfiguration.responds(to: implementNetfoxSelector) {
+            URLSessionConfiguration.perform(implementNetfoxSelector)
+        }
+    }
+}
