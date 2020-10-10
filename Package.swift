5bea46a77383d07ab257ad48768e44ddb027dbed
Garett <garett.breen@gmail.com>
2020-10-10 11:15:32 -0600

    Trying to support macOS

--- Package.swift
+++ Package.swift
@@ -8,8 +8,12 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "NetFox",
-            targets: ["NetFox-iOS", "NetFox-macOS"]),
+            name: "NetFox-iOS",
+            targets: ["NetFox-iOS"]
+        ),
+        .library(
+            name: "NetFox-macOS",
+            targets: ["NetFox-macOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
