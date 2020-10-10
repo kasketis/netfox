d49a3eeabfeb67be63d8ac1e1f5697915ccacf1f
Garett <garett.breen@gmail.com>
2020-10-10 11:33:44 -0600

    Revert

--- Package.swift
+++ Package.swift
@@ -8,12 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "NetFox-iOS",
-            targets: ["NetFox-iOS"]
-        ),
-        .library(
-            name: "NetFox-macOS",
-            targets: ["NetFox-macOS"]),
+            name: "NetFox",
+            targets: ["NetFox"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,19 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "NetFox-Core",
-            path: "netfox/Core",
-            exclude: ["NFXLoader.h", "NFXLoader.m"]
-        ),
-        .target(
-            name: "NetFox-iOS",
-            dependencies: ["NetFox-Core"],
-            path: "netfox/iOS"
-        ),
-        .target(
-            name: "NetFox-macOS",
-            dependencies: ["NetFox-Core"],
-            path: "netfox/OSX"
+            name: "NetFox",
+            path: "netfox",
+            exclude: ["Core/NFXLoader.h", "Core/NFXLoader.m", "OSX"]
         ),
     ]
 )
