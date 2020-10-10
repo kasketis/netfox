c70f0e640454678d233380c5aea561d13ad4da9b
Garett <garett.breen@gmail.com>
2020-10-10 10:52:39 -0600

    Updates to package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NetFox",
-            targets: ["NetFox"]),
+            targets: ["NetFox-iOS", "NetFox-macOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,9 +19,19 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "NetFox",
-            path: "netfox",
-            exclude: ["Core/NFXLoader.h", "Core/NFXLoader.m", "OSX"]
+            name: "NetFox-Core",
+            path: "netfox/Core",
+            exclude: ["NFXLoader.h", "NFXLoader.m"]
+        ),
+        .target(
+            name: "NetFox-iOS",
+            dependencies: ["NetFox-Core"],
+            path: "netfox/iOS"
+        ),
+        .target(
+            name: "NetFox-macOS",
+            dependencies: ["NetFox-Core"],
+            path: "netfox/OSX"
         ),
     ]
 )
