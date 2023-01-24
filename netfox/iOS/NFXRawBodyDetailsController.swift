84fa395dcf7ffe18a0d0c09516693a49016d5eba
Arman Turalin <tursat1@mail.ru>
2023-01-24 20:54:52 +0100

    Configure search at raw body screen

--- netfox/iOS/NFXRawBodyDetailsController.swift
+++ netfox/iOS/NFXRawBodyDetailsController.swift
@@ -14,12 +14,29 @@ class NFXRawBodyDetailsController: NFXGenericBodyDetailsController {
     var bodyView: UITextView = UITextView()
     private var copyAlert: UIAlertController?
     
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.delegate = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.autoresizingMask = [.flexibleWidth]
+        searchController.searchBar.backgroundColor = UIColor.clear
+        searchController.searchBar.barTintColor = UIColor.NFXOrangeColor()
+        searchController.searchBar.tintColor = UIColor.NFXOrangeColor()
+        searchController.searchBar.searchBarStyle = .minimal
+        searchController.view.backgroundColor = UIColor.clear
+        return searchController
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let viewFrame = view.frame
         
         title = "Body details"
         
+        configureNavigationBar()
+
         bodyView.frame = CGRect(x: 0, y: 0, width: viewFrame.width, height: viewFrame.height)
         bodyView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         bodyView.backgroundColor = UIColor.clear
@@ -33,13 +50,32 @@ class NFXRawBodyDetailsController: NFXGenericBodyDetailsController {
         bodyView.addGestureRecognizer(lpgr)
         
         switch bodyType {
-            case .request:
-                bodyView.text = selectedModel.getRequestBody() as String
-            default:
-                bodyView.text = selectedModel.getResponseBody() as String
+        case .request:
+            bodyView.attributedText = formatNFXString(selectedModel.getRequestBody())
+        default:
+            bodyView.attributedText = formatNFXString(selectedModel.getResponseBody())
         }
         
         view.addSubview(bodyView)
+    }
+    
+    private func configureNavigationBar() {
+        if #available(iOS 11.0, *) {
+            navigationItem.searchController = searchController
+            navigationItem.hidesSearchBarWhenScrolling = false
+            definesPresentationContext = true
+        } else {
+            let searchView = UIView()
+            searchView.frame = CGRect(x: 0, y: 0, width: view.frame.width - 60, height: 0)
+            searchView.autoresizingMask = [.flexibleWidth]
+            searchView.autoresizesSubviews = true
+            searchView.backgroundColor = UIColor.clear
+            searchView.addSubview(searchController.searchBar)
+            searchController.searchBar.sizeToFit()
+            searchView.frame = searchController.searchBar.frame
+
+            navigationItem.titleView = searchView
+        }
     }
 
     @objc fileprivate func copyLabel(lpgr: UILongPressGestureRecognizer) {
@@ -65,6 +101,24 @@ class NFXRawBodyDetailsController: NFXGenericBodyDetailsController {
     @objc fileprivate func dismissCopyAlert() {
         copyAlert?.dismiss(animated: true) { [weak self] in self?.copyAlert = nil }
     }
+}
+
+// MARK: - UISearchResultsUpdating
+extension NFXRawBodyDetailsController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchString = searchController.searchBar.text ?? ""
+        
+        switch bodyType {
+        case .request:
+            bodyView.attributedText = formatNFXString(selectedModel.getRequestBody(), searchString: searchString)
+        default:
+            bodyView.attributedText = formatNFXString(selectedModel.getResponseBody(), searchString: searchString)
+        }
+    }
+}
+
+// MARK: - UISearchControllerDelegate
+extension NFXRawBodyDetailsController: UISearchControllerDelegate {
 }
 
 #endif
